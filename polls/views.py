83f5679d0a02dcad5338ef7f06575eec9f6c873f
Al-ameen Abiola <alameenabiola3@gmail.com>
2025-10-29 08:42:16 +0100

    imported loader module

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 
+from django.template import loader
+
 from .models import Question
 
 
@@ -8,7 +10,7 @@ def index(request):
     template = loader.get_template("polls/index.html")
     context = {"latest_question_list": latest_question_list}
     # output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
