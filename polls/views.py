2d5215e7950bd9b80360fcc5c0a90d2300ed5861
Al-ameen Abiola <alameenabiola3@gmail.com>
2025-10-29 08:56:58 +0100

    raising a 404 error

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,5 @@
+from django.http import Http404
 from django.shortcuts import render
-
-
 
 from .models import Question
 
@@ -15,4 +14,16 @@ The render() function takes the request object as its first argument,
 a template name as its second argument and a dictionary as its optional 
 third argument. It returns an HttpResponse object of the given template 
 rendered with the given context.
+'''
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question":question})
+
+'''
+The view raises the Http404 exception if a question with the requested 
+ID doesn’t exist.
 '''
